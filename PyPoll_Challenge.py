c7ef32fedc4f07147e1f5649e8bf3749fcb171c8
DESKTOP-2HA3E7O\jill <jillianjagoda@gmail.com>
2021-09-11 16:45:43 -0500

    fixing challenge

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -99,7 +99,7 @@ with open(file_to_save, "w") as txt_file:
 
          # 6d: Print the county results to the terminal.
         county_results= (
-            f"{county_name}: {vote_percentage:.2f}% ({total_votes:,})\n")
+            f"{county_name}: {vote_percentage:.2f}% ({votes:,})\n")
         print(county_results)
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
