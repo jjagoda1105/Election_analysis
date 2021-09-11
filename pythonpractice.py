3ffa929c6a6b10e2c90b8b4e347b6fcb0175369c
DESKTOP-2HA3E7O\jill <jillianjagoda@gmail.com>
2021-09-11 13:37:51 -0500

    Adding PyPoll.py file.

---
+++ pythonpractice.py
@@ -0,0 +1,13 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+for county in counties:
+    print(county)
+for num in range(5):
+    print(num)
+for county in counties_dict.keys():
+    print(county)
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict.keys():
+    print(county)
+  
